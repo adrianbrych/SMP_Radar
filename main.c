5deabe3c7f498f1c713c4a49903940a59e7268a0
adrianbrych <73898292+adrianbrych@users.noreply.github.com>
2021-01-23 16:27:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * This file is a part of the SM2 Project				                              *
+ ******************************************************************************/
+
+/**
+ * @file main.c
+ * @author Banys & Brychner
+ * @date Jan 2021
+ * @brief File containing main function of the program.
+ * @ver 1
+ */
+
+
+
+//Headers to include
 #include "frdm_bsp.h" 
 #include "tpm1.h"
 #include "tpm0.h"
@@ -5,48 +20,40 @@
 #include "lcd1602.h"
 #include "MKL05Z4.h" /*Device header*/
 
-void delay_ms( int n) {
-volatile int i;
-volatile int j;
-							for( i = 0 ; i < n; i++)
-							for(j = 0; j < 3500; j++) {}
-}
-
+//Variables used to send data via UART
 uint32_t i=0;
-
+uint32_t pomiar = 0;
 char rx_buf[]={0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
 
-void send(uint32_t val, char* poz){
-	TPM1_SetVal(val);	// 250, 500, 750
-		sprintf(rx_buf,"%s", poz);
-		for(i=0;rx_buf[i]!=0;i++)
-		{
-			while(!(UART0->S1 & UART0_S1_TDRE_MASK));
-			UART0->D = rx_buf[i];
+
+//Send servo position and data from HC-SR04 via UART
+void send(uint32_t val, char* poz)
+	{
+	TPM1_SetVal(val);														// Set servo in a specific position
+	sprintf(rx_buf,"%s", poz);
+	for(i=0;rx_buf[i]!=0;i++){
+		while(!(UART0->S1 & UART0_S1_TDRE_MASK));
+		UART0->D = rx_buf[i];											// Send position of servo via UART
 		}
-			TRIG_inpulse();
-			int x = TPM0_GetVal();
-			sprintf(rx_buf,"%d%c",x/19,0xd);
-			for(i=0;rx_buf[i]!=0;i++)
-			{
-				while(!(UART0->S1 & UART0_S1_TDRE_MASK));
-				UART0->D = rx_buf[i];
-			}
+	
+	TRIG_inpulse();
+	pomiar = TPM0_GetVal();											// Get data from HC-SR04
+	sprintf(rx_buf,"%d%c",pomiar/19,0xd);
+	for(i=0;rx_buf[i]!=0;i++){
+		while(!(UART0->S1 & UART0_S1_TDRE_MASK));
+		UART0->D = rx_buf[i];											// Send data via UART
+		}
 	delay_ms(200);
 	}
 			
-	
-
 
 
 int main (void) { 
 	UART0_Init();
-	TPM0_Init_InputCapture();	    // TPM0 initialization						
-	//TPM0_Init_OutputCompare();
+	TPM0_Init_InputCapture();						
 	TPM1_Init();
-	TRIG_GPIO_Init();
+	TRIG_GPIO_Init();					// Initialize UART, TPMs, and GPIO
 	
-
   while(1) {
 		send(250, "a");
 		delay_ms(250);
@@ -71,7 +78,7 @@ int main (void) {
 		send(410, "c");
 		delay_ms(250);
 		send(330, "b");
-		delay_ms(250);
+		delay_ms(250);				// Loop servo from -90 degs to 90 degs with 7 steps
 	}	
 }									
-							
+						
