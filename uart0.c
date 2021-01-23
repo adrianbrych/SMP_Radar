59dba28a322cbdda45869a8ee3a3b7adc09d03f9
adrianbrych <73898292+adrianbrych@users.noreply.github.com>
2021-01-23 16:30:52 +0100

    Update uart0.c

--- uart0.c
+++ uart0.c
@@ -2,18 +2,21 @@
 
 void UART0_Init(void)
 {
-	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;							//UART0 dołączony do zegara
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;							//Port B dołączony do zegara
-	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(MCGFLLCLK);		//Zegar MCGFLLCLK=41943040Hz (CLOCK_SETUP=0)
-	PORTB->PCR[1] = PORT_PCR_MUX(2);								//PTB1=TX_D
-	PORTB->PCR[2] = PORT_PCR_MUX(2);								//PTB2=RX_D
+	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;				// Connect UART0 to clock
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;				// Connect PORT B to clock
+	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(MCGFLLCLK);		        // Set MCGFLLCLK=41943040Hz (CLOCK_SETUP=0) as a clock source
+	PORTB->PCR[1] = PORT_PCR_MUX(2);				// PTB1 = TX_D
+	PORTB->PCR[2] = PORT_PCR_MUX(2);				// PTB2 = RX_D
 	
-	UART0->C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK );		//Blokada nadajnika i o dbiornika
+	UART0->C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK );		// Block transmitter and receiver
 	UART0->BDH = 0;
-	UART0->BDL =91;		//Dla CLOCK_SETUP=0 BR=28800	BDL=91	:	CLOCK_SETUP=1	BR=230400		BDL=13
+	UART0->BDL =91;																					// For CLOCK_SETUP=0, BR=28800,	BDL=91
 	UART0->C4 &= ~UART0_C4_OSR_MASK;
-	UART0->C4 |= UART0_C4_OSR(15);	//Dla CLOCK_SETUP=0 BR=28800	OSR=15	:	CLOCK_SETUP=1	BR=230400		OSR=15
-	UART0->C5 |= UART0_C5_BOTHEDGE_MASK;	//Próbkowanie odbiornika na obydwu zboczach zegara
-	UART0->C2 |= UART0_C2_TE_MASK;		//Włącz nadajnik
+	UART0->C4 |= UART0_C4_OSR(15);					//For CLOCK_SETUP=0, BR=28800	OSR=15
+	
+	
+	UART0->C5 |= UART0_C5_BOTHEDGE_MASK;				// Sampling on both edges
+	UART0->C2 |= UART0_C2_TE_MASK;					// Transmitter on
 }
+
 
