f409dc037309a0d27252ef80691d7e307399eb7f
adrianbrych <73898292+adrianbrych@users.noreply.github.com>
2021-01-23 16:33:09 +0100

    Update uart0.h

--- uart0.h
+++ uart0.h
@@ -1,12 +1,26 @@
+/******************************************************************************
+ * This file is a part of the SM2 Project				                              *
+ ******************************************************************************/
+
+/**
+ * @file uart0.h
+ * @author Banys & Brychner
+ * @date Jan 2021
+ * @brief File containing enums, structures and declarations for UART0.
+ * @ver 1
+ */
+ 
+#ifndef TPM_H
+#define TPM_H
+ 
 #include "MKL05Z4.h"
-//----------------------------------------------
-// SIM_SOPT2
-//----------------------------------------------
-// Źródło zegara
-//
+
 #define CLK_DIS 					0x00
 #define MCGFLLCLK 				0x01
 #define OSCERCLK					0x02
 #define MCGIRCLK					0x03
 
+// Initialization of UART
 void UART0_Init(void);
+
+#endif
